7ebf1be414e2bd001bcbe863cb5ed71c77e06f4d
Anoop Dixith <adixith@conviva.com>
2015-04-03 14:15:03 -0700

    Updated create directories script to create SCHEDULES. Found out the version provided by the website was after 96th Amendment. Updated the bundle

--- create_directories.sh
+++ create_directories.sh
@@ -1,7 +1,13 @@
 number_of_parts=22
+number_of_schedules=12
 prefix="PART_"
+prefix_for_schedules="SCHEDULE_"
+
 
 for((i=1;i<=$number_of_parts;i++))
 do
-	mkdir "$prefix$i"	
+	#mkdir "$prefix$i"
+	if [ $i -le $number_of_schedules ]; then
+		mkdir "$prefix_for_schedules$i"
+	fi	
 done
