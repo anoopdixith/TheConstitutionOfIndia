5724606067114a1d4395ca652e6c8940470dce7b
Modified1: Committing the file that is being used to delegate some of the git chores <>
2015-04-16 17:30:45 -0700

    Modified1: Committing the file that is being used to delegate some of the git chores.

--- commit_chores.sh
+++ commit_chores.sh
@@ -1,6 +1,7 @@
 #This script takes care of committing changes and pushing it to the remote.
 #Pass the commit message as the first argument.
 #If applicable, send author's name as the second argument.
+#Comment modification 1: Using this file to test this same file.
 message=$1;
 git add *;
 if [[ $# -eq 2 ]] ; then
